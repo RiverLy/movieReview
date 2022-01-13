78c9087910ccc73ceec3f6c09c5b5b615d414088
RiverLy <emteofh0313@naver.com>
2022-01-13 16:38:30 +0900

    [FEAT] Review repository method for getting reviews by movie added

--- src/main/java/com/river/movieReview/repository/ReviewRepository.java
+++ src/main/java/com/river/movieReview/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.river.movieReview.repository;
 
+import com.river.movieReview.entity.Movie;
 import com.river.movieReview.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByMovie(Movie movie);
+
 }
