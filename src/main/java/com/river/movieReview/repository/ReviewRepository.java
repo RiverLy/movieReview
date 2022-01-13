83c315023b6e2dcf56420432727ea1aabfd1972f
RiverLy <emteofh0313@naver.com>
2022-01-13 16:50:07 +0900

    [FEAT] Review repository method for deleting reviews by member added

--- src/main/java/com/river/movieReview/repository/ReviewRepository.java
+++ src/main/java/com/river/movieReview/repository/ReviewRepository.java
@@ -1,9 +1,12 @@
 package com.river.movieReview.repository;
 
+import com.river.movieReview.entity.Member;
 import com.river.movieReview.entity.Movie;
 import com.river.movieReview.entity.Review;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +14,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
+
+    @Modifying
+    @Query("delete from Review r where r.member = :member")
+    void DeleteByMember(Member member);
 
 }
