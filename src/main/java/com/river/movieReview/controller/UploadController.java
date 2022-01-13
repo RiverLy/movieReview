2f0ed1b43fd69b74c43e74b6b2ce1cb97c47fd12
RiverLy <emteofh0313@naver.com>
2022-01-13 17:45:02 +0900

    [FEAT] method for showing uploaded image added

--- src/main/java/com/river/movieReview/controller/UploadController.java
+++ src/main/java/com/river/movieReview/controller/UploadController.java
@@ -1,17 +1,27 @@
 package com.river.movieReview.controller;
 
+import com.river.movieReview.dto.UploadResultDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Log4j2
@@ -21,14 +31,44 @@ public class UploadController {
     @Value("${com.river.upload.path}")
     private String uploadPath;
 
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            log.info("fileName : " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file : " + file);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
+
+        } catch (Exception e){
+
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+        return result;
+
+    }
+
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploads){
+    public ResponseEntity<List<UploadResultDto>> uploadFile(MultipartFile[] uploads){
+
+        List<UploadResultDto> resultDtoList = new ArrayList<>();
 
         for(MultipartFile file : uploads){
 
             if(file.getContentType().startsWith("image") == false){
                 log.warn("The file should be Image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             String originalName = file.getOriginalFilename();
@@ -45,10 +85,14 @@ public class UploadController {
 
             try {
                 file.transferTo(savePath);
+                resultDtoList.add(new UploadResultDto(fileName, uuid, folderPath));
             } catch (IOException e){
-                log.error(e);
+                log.error(e.getMessage());
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
+
+        return new ResponseEntity<>(resultDtoList, HttpStatus.OK);
 
     }
 
