0c48b2ba2d6faced518ed3cc99902ad5fd0098c5
RiverLy <emteofh0313@naver.com>
2022-01-13 16:26:35 +0900

    [FEAT] Movie repository method for getting specific movie with all related data added

--- src/main/java/com/river/movieReview/repository/MovieRepository.java
+++ src/main/java/com/river/movieReview/repository/MovieRepository.java
@@ -6,11 +6,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    @Query("select m, i, avg(coalesce(r.grade,0)), count(distinct r) from Movie m "
+    @Query("select m, i, avg(coalesce(r.grade,0)), count(r) from Movie m "
             + "left outer join MovieImage i on i.movie = m "
             + "left outer join Review r on r.movie = m group by m")
     Page<Object[]> getListPage(Pageable pageable);
+
+    @Query("select m, i, avg(coalesce(r.grade,0)), count(r) from Movie m "
+            + "left outer join MovieImage i on i.movie = m "
+            + "left outer join Review r on r.movie = m "
+            + "where m.mno = :mno group by i")
+    List<Object[]> getMovieWithAll(Long mno);
 
 }
