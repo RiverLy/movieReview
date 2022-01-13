871d0c230f7013b1e594bf09739ef7fc3a27a47a
RiverLy <emteofh0313@naver.com>
2022-01-13 16:44:42 +0900

    [FEAT] EAGER fetch member when getting reviews by movie

--- src/main/java/com/river/movieReview/repository/ReviewRepository.java
+++ src/main/java/com/river/movieReview/repository/ReviewRepository.java
@@ -2,12 +2,14 @@ package com.river.movieReview.repository;
 
 import com.river.movieReview.entity.Movie;
 import com.river.movieReview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
 
 }
